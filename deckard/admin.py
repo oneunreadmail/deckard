46dd6c28058696fd97a494e8d1830edb273a4d0f
oakenvial <julia.nikolaeva@live.com>
2017-12-05 20:17:25 +0300

    Added models Post, Person, Comment, Blog, Like, Image (draft version)

--- deckard/admin.py
+++ deckard/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 
 admin.site.register(Post)
-# Register your models here.
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Blog)
+admin.site.register(Image)
+admin.site.register(Person)
--- deckard/models.py
+++ deckard/models.py
@@ -1,14 +1,87 @@
 from django.db import models
-from django.core.urlresolvers import reverse
-import random
+from django import VERSION
+from django.utils import timezone
+from django.contrib import auth
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+if VERSION[0] == 2:  # Starting from Django 2.0 reverse is located in django.urls
+    from django.urls import reverse
+else:
+    from django.core.urlresolvers import reverse
+
+class SystemInfo(models.Model):
+    """Abstract base class for storing system information about a record."""
+    # Created by author (indexed)
+    author = models.ForeignKey('auth.User',
+                               verbose_name='author',
+                               db_index=True,  # Indexed
+                               related_name='created_%(class)ss',
+                               on_delete=models.SET_NULL,
+                               null=True)
+    # Modified by
+    modified_by = models.ForeignKey('auth.User',
+                                    verbose_name='modified by',
+                                    db_index=False,  # Index not needed
+                                    related_name='modified_%(class)ss',
+                                    on_delete=models.SET_NULL,
+                                    null=True)
+    created_date = models.DateTimeField(verbose_name='creation date',
+                                        default=timezone.now,
+                                        blank=True)
+    modified_date = models.DateTimeField(verbose_name='modification date',
+                                         auto_now=True,
+                                         blank=True)
+
+    class Meta:
+        abstract = True
 
 
-class Post(models.Model):
+class Person(models.Model):
+    """A person registered in the system."""
+    user = models.OneToOneField(auth.models.User,
+                                on_delete=models.CASCADE,
+                                null=True)
+    first_name = models.CharField(verbose_name='first name',
+                                  max_length=50)
+    last_name = models.CharField(verbose_name='last name',
+                                 max_length=50)
+    # Middle name or patronymic
+    middle_name = models.CharField(verbose_name='middle name',
+                                   max_length=50,
+                                   null=True,
+                                   blank=True)
+    phone = models.CharField(verbose_name='contact phone number',
+                             max_length=20,
+                             null=True,
+                             blank=True)
+    email = models.EmailField(verbose_name='e-mail address',
+                              null=True,
+                              blank=True)
+    district = models.CharField(verbose_name='district',
+                                max_length=50,
+                                null=True,
+                                blank=True)
+
+    def __str__(self):
+        return 'Person ' + str(self.id) + ' ' + self.first_name + ' ' + self.last_name
+
+    # The next part should be reviewed. Is this really necessary?
+    @receiver(post_save, sender=auth.models.User)
+    def create_or_update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Person.objects.create(user=instance)
+        instance.person.save()
+
+
+class Post(SystemInfo):
+    """A text entry which belongs to a number of blogs."""
     title = models.CharField(max_length=140)
     text = models.TextField()
-    #slug = models.SlugField(default=str(random.random()))
-    dt_update = models.DateTimeField(auto_now=True, auto_now_add=False)
-    dt_create = models.DateTimeField(auto_now=False, auto_now_add=True)
+    published_date = models.DateTimeField(verbose_name='publication date',
+                                          blank=True,
+                                          null=True)
+    slug = models.SlugField(blank=True,
+                            null=True)
 
     def __str__(self):
         return self.title
@@ -17,4 +90,72 @@ class Post(models.Model):
         return reverse("get_post", kwargs={"post_id": self.id})
 
 
-# Create your models here.
+class Comment(SystemInfo):
+    """A comment to a post, can have child comments."""
+    COMMENT_STATUS = (
+        ('PN', 'Pending'),
+        ('AP', 'Approved'),
+        ('RJ', 'Rejected'),
+    )
+    parent_comment = models.ForeignKey('Comment',
+                                       verbose_name='parent comment',
+                                       db_index=True,  # Indexed
+                                       related_name='child_%(class)ss',
+                                       on_delete=models.CASCADE,
+                                       null=True,  # Root comment has no parent
+                                       blank=True)
+    text = models.TextField(verbose_name='text')
+    status = models.CharField(verbose_name='status',
+                              max_length=60,
+                              choices=COMMENT_STATUS,
+                              default='Pending')
+
+    def __str__(self):
+        return self.text[:50] + ' (' + self.status + ')'
+
+
+class Blog(models.Model):
+    """Blog is a logical group of posts written by the same or different authors."""
+    name = models.CharField(verbose_name='name',
+                            max_length=140)
+    posts = models.ManyToManyField(Post,
+                                   blank=True)
+    contributors = models.ManyToManyField(Person,
+                                          blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Like(SystemInfo):
+    """A like is a sign of approval issued by a user. Post and comments both can be liked."""
+    post_id = models.ForeignKey('Post',
+                                verbose_name='post',
+                                db_index=True,
+                                related_name='liked_%(class)ss',
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True)
+    comment_id = models.ForeignKey('Comment',
+                                   verbose_name='comment',
+                                   db_index=True,
+                                   related_name='liked_%(class)ss',
+                                   on_delete=models.CASCADE,
+                                   null=True,
+                                   blank=True)
+
+
+class Image(models.Model):
+    """An image belongs to a post."""
+    post_id = models.ForeignKey('Post',
+                                verbose_name='post',
+                                db_index=True,
+                                related_name='images_in_a_%(class)ss',
+                                on_delete=models.CASCADE,
+                                null=False)
+    position = models.IntegerField(verbose_name='position',
+                                   default=0,
+                                   null=False)
+    link = models.CharField(verbose_name='image link',
+                            max_length=100,
+                            null=False)
