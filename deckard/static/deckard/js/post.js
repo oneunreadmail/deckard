b2d2da5679de10c275276f27cb65c33fccc0ab5e
oakenvial <julia.nikolaeva@live.com>
2017-12-15 16:31:14 +0300

    Added green and red highlighting for rating icons. Refactored views

--- deckard/static/deckard/js/post.js
+++ deckard/static/deckard/js/post.js
@@ -4,7 +4,13 @@ $(function(){
          $.ajax({
              url: $(this).attr('data-link'),
              success: $.proxy(function(newRating){
-                $(this).parents('.dkr-vertical-rating').eq(0).find('.dkr-rating').eq(0).text(newRating);
+             var parent = $(this).parents('.dkr-vertical-rating').eq(0);
+                oldRating = parent.find('.dkr-rating').eq(0).text();
+                if (newRating != oldRating) {
+                    parent.find('.dkr-active').eq(0).removeClass('dkr-active');
+                    $(this).toggleClass('dkr-active', (newRating != oldRating) && (newRating != 0));
+                    parent.find('.dkr-rating').eq(0).text(newRating);
+                }
              }, this)
          })
       });
--- deckard/views.py
+++ deckard/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect, HttpResponse
-from .models import Post, Blog, BlogPost, Comment
+from .models import Post, Blog, BlogPost, Comment, Rating
 from .forms import PostCreateForm
 from django.db.models import Sum
 
@@ -12,32 +12,60 @@ def check(request):  # function for current testing
     return render(request, 'deckard/comments.html', context)
 
 
-def blog_posts(request, blog_name):
-    """List of all posts in the current blog."""
-    blog = get_object_or_404(Blog, name=blog_name)
-    blogs = Blog.objects.all()
-    blogposts = BlogPost.objects.filter(blog__name=blog_name).order_by("-pinned", "-published_date")
-    ratings = {}
-    for blogpost in blogposts:
-        sum_rating = blogpost.post.posts_ratings.aggregate(Sum('points'))['points__sum']
-        ratings[blogpost.post.id] = sum_rating
-
-    context = {
-        "blog": blog,
-        "blogs": blogs,
-        "blogposts": blogposts,
-        "ratings": ratings,
-        "user": request.user,
-    }
-    return render(request, 'deckard/blog_posts.html', context)
-
-
 def blog_list(request):
     """List of all created blogs."""
     context = {
         "blogs": Blog.objects.all(),
     }
     return render(request, 'deckard/blog_list.html', context)
+
+
+def blog_posts(request, blog_name):
+    """List of all posts in the current blog."""
+    blog = get_object_or_404(Blog, name=blog_name)
+    blogposts = BlogPost.objects.filter(blog=blog).order_by("-pinned", "-published_date")
+
+    ratings = {}
+    for blogpost in blogposts:
+        ratings[blogpost.post] = get_rating(blogpost.post, request.user)
+
+    context = {
+        "user": request.user,
+        "blog": blog,
+        "blogposts": blogposts,
+        "ratings": ratings,
+    }
+    return render(request, 'deckard/blog_posts.html', context)
+
+
+def get_post(request, post_id, blog_name):
+    """List of all posts in the current blog."""
+    blogpost = get_object_or_404(BlogPost, blog__name=blog_name, post__id=post_id)
+    comments = Comment.objects.filter(post_id=post_id).order_by("position")
+
+    ratings = {}
+    ratings[blogpost.post.id] = get_rating(blogpost.post, request.user)
+
+    context = {
+        "user": request.user,
+        "blog": blogpost.blog,
+        "blogpost": blogpost,
+        "ratings": ratings,
+        "comments": comments,
+    }
+    return render(request, 'deckard/post_detail.html', context)
+
+
+def get_rating(post, user):
+    """Get post sum rating and post user rating as a tuple (SUM_RT, USER_RT)."""
+    sum_rating = post.posts_ratings.aggregate(Sum('points'))['points__sum']
+    post_user_rating = Rating.objects.filter(post=post, author=user)
+    if post_user_rating:
+        user_points = post_user_rating.first().points
+    else:
+        user_points = 0
+    rating = (sum_rating, user_points)
+    return rating
 
 
 def add_new_post(request, blog_name):
@@ -85,18 +113,6 @@ def edit_post(request, post_id, blog_name):
         "blog": get_object_or_404(Blog, name=blog_name),
     }
     return render(request, 'deckard/create_update_post.html', context)
-
-
-def get_post(request, post_id, blog_name):
-    blogpost = get_object_or_404(BlogPost, blog__name=blog_name, post__id=post_id)
-    comments = Comment.objects.filter(post_id=post_id).order_by("position")
-    context = {
-        "blogpost": blogpost,
-        "blog": blogpost.blog,
-        "comments": comments,
-        "user": request.user,
-    }
-    return render(request, 'deckard/post_detail.html', context)
 
   
 def delete_post(request, post_id, blog_name):
