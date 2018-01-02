e822757b1b805be3d60ae5501c3017fdc5251d91
oneunreadmail <github@oneunreadmail.com>
2018-01-03 01:34:55 +0300

    added cut functionality for posts

--- deckard/jinja2.py
+++ deckard/jinja2.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from django.utils import translation
 from jinja2 import Environment
-from deckard.templatetags.filters import youtube
+from deckard.templatetags import filters
 
 
 def reverse_upgrade(x, *args, **kwargs):
@@ -16,5 +16,7 @@ def environment(**options):
         'static': staticfiles_storage.url,
         'url': reverse_upgrade,
     })
-    env.filters['youtube'] = youtube
+    env.filters['youtube'] = filters.youtube
+    env.filters['collapsed'] = filters.collapsed
+    env.filters['expanded'] = filters.expanded
     return env
